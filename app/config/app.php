266637f42eedb3fd9792f79b510460d851cce989
YongHun Byun <yonghunbyun@gmail.com>
2014-03-29 13:01:26 +0900

    asset-pipeline, facebook php sdk 패키지 추가

--- app/config/app.php
+++ app/config/app.php
@@ -107,6 +107,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
+        'Codesleeve\AssetPipeline\AssetPipelineServiceProvider',
 	),
 
 	/*
