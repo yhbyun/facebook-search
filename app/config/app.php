90e58fe6019c6a71485e43b7ce0dfe663dcc9653
YongHun Byun <yonghunbyun@gmail.com>
2014-03-29 13:11:15 +0900

    idehelper 패키지 추가

--- app/config/app.php
+++ app/config/app.php
@@ -108,6 +108,7 @@ return array(
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
         'Codesleeve\AssetPipeline\AssetPipelineServiceProvider',
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
 	),
 
 	/*
