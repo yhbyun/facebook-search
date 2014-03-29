525bf781ffbb643bec1440454b9413416fdb7079
YongHun Byun <yonghunbyun@gmail.com>
2014-03-29 13:31:46 +0900

    database 설정 추가

---
+++ app/config/local/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+    'connections' => array(
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'facebook',
+            'username'  => 'root',
+            'password'  => 'root',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+    )
+);
+
+
+ 
---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    'connections' => array(
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'facebook',
+            'username'  => 'facebook',
+            'password'  => 'facebookwebuser',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+    )
+);
+
