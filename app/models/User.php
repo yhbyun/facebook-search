ca0c3d727f1a2388bfc1e68c767729c7cf3c779f
YongHun Byun <yonghunbyun@gmail.com>
2014-03-29 14:54:51 +0900

    User 모델 수정, Profile 모델 추가

--- app/models/User.php
+++ app/models/User.php
@@ -49,4 +49,8 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 		return $this->email;
 	}
 
+    public function profiles()
+    {
+        return $this->hasMany('Profile');
+    }
 }
