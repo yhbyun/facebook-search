4584f76d0f33dd082027d7ccc580eb00dfa0324d
YongHun Byun <yonghunbyun@gmail.com>
2014-03-29 22:47:33 +0900

    named route로 변경

--- app/routes.php
+++ app/routes.php
@@ -3,9 +3,9 @@
 Route::pattern('id', '[a-z0-9\-]+');
 
 Route::group(array('prefix' => ''), function() {
-    Route::get('', 'FacebookController@getIndex');
-    Route::get('login', 'FacebookController@getLogin');
-    Route::get('login/callback', 'FacebookController@getCallback');
-    Route::get('logout', 'FacebookController@getLogout');
+    Route::get('', ['as' => 'facebook.main', 'uses' => 'FacebookController@getIndex']);
+    Route::get('login', ['as' => 'facebook.login', 'uses' => 'FacebookController@getLogin']);
+    Route::get('login/callback', ['as' => 'facebook.callback', 'uses' => 'FacebookController@getCallback']);
+    Route::get('logout', ['as' => 'facebook.logout', 'uses' => 'FacebookController@getLogout']);
     Route::get('posts/{id}', ['as' => 'facebook.posts', 'uses' => 'FacebookController@getPosts']);
 });
